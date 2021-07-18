42ab1346842fc2fb3ecb17be2be381ba27a525ea
Ronan-Codes <galv.ronan@gmail.com>
2021-07-18 17:20:48 -0400

    Installed packages

--- public/js/idb.js
+++ public/js/idb.js
@@ -10,3 +10,28 @@ request.onupgradeneeded = function(event) {
     // create an object store(table) `new_transaction`, & set to have auto incrementing primary key of sorts
     db.createdObjectStore('new_transaction', { autoIncrement: true });
 }
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        // uploadTransaction()
+    }
+}
+
+request.onerror= function(event) {
+
+}
+
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
+// Insert saveRecord in. . . . form submission if fetch() function's catch method is executed
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    // access the object store for `new_transaction`
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    // add record to your store with add method
+    budgetObjectStore.add(record);
+}
